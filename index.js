4136ac1fc4b8b1f5a262209c4e9ff6778a38d9c1
rahal <rahalncm@gmail.com>
2022-07-28 00:41:12 -0400

    basic server added

---
+++ index.js
@@ -0,0 +1,9 @@
+const express = require('express');
+var mongoose = require('mongoose');
+/*var bcrypt = require('bcryptjs');
+var bodyParser = require('body-parser');
+var secretkey = require('./config/configurations').JWTKey;*/
+var jwt = require('jsonwebtoken');
+var app = express();
+
+app.listen(3000);
