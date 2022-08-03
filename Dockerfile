b89537e7a078904db5dbac4ef3e753971ac7bd35
rahal <rahalncm@gmail.com>
2022-08-03 10:01:59 -0400

    docker-ignore file added

--- Dockerfile
+++ Dockerfile
@@ -1,6 +1,11 @@
 FROM node:16
 WORKDIR /sales-tray-backend
 COPY package.json .
+RUN npm install -g nodemon
 RUN npm install
 COPY . .
-CMD npm start
+
+# Exports
+EXPOSE 3000
+CMD [ "npm", "run", "dev" ]
+#CMD npm start
--- src/index.js
+++ src/index.js
@@ -4,6 +4,15 @@ var mongoose = require('mongoose');
 var bodyParser = require('body-parser');
 var secretkey = require('./config/configurations').JWTKey;*/
 var jwt = require('jsonwebtoken');
-var app = express();
 
-app.listen(3000);
+console.log('just changed it');
+
+
+const { PORT = '3000' } = process.env
+const app = express()
+
+app.use((req, res, next) => {
+    res.send('<h4>Backend is up </h4>')
+})
+
+app.listen(PORT)
