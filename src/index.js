339b8d1d2adb8dc9434783d56ce7b0ad67bd98b4
rahal <rahalncm@gmail.com>
2022-08-03 14:02:57 -0400

    basic implementations for REST API with db check

--- src/index.js
+++ src/index.js
@@ -4,6 +4,7 @@ var bodyParser = require('body-parser');
 var secretkey = require('./config/configurations').JWTKey;*/
 var jwt = require('jsonwebtoken');
 var mysql = require('mysql');
+const app = express()
 
 console.log('just changed it');
 
@@ -21,9 +22,32 @@ con.connect(function(err) {
     console.log("DB Connected!");
 });
 
+app.get('/', function(req, res){
+    res.send('<h4>Backend is up </h4>');
+});
+
+app.get('/check_db', async function (req, res) {
+    const dbResponse = await checkDBConnection();
+    console.log('response received from server query: ', dbResponse);
+    res.send(dbResponse);
+});
+
+function checkDBConnection() {
+    return new Promise((resolve, reject)=> {
+        con.query("SELECT 'DB is UP' as Result", function (err, result, fields) {
+            if (err) throw err;
+            console.log('fromDB: ', result)
+            resolve(JSON.stringify(result));
+        });
+    });
+}
+
+app.get('*', function(req, res){
+    res.send('Sorry, this is an invalid URL.');
+});
+
 
 const { PORT = '3000' } = process.env
-const app = express()
 
 app.use((req, res, next) => {
     res.send('<h4>Backend is up </h4>')
