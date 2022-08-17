41dbb48cac5f95bb05162e5a649a0d4c72bb2389
Rahal Danthanarayana <rahalkanishka@gmail.com>
2022-08-17 06:23:19 -0400

    introduced a interface for controller functions

---
+++ src/main/java/com/salestray/salestraybackend/controllers/BasicController.java
@@ -0,0 +1,58 @@
+package com.salestray.salestraybackend.controllers;
+
+import com.salestray.salestraybackend.entities.Item;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import java.util.List;
+
+/**
+ * interface with basic controller functions
+ *
+ * @param <T>
+ */
+public interface BasicController<T> {
+
+    /**
+     * get all the objects as a list
+     *
+     * @return all objects
+     */
+    @ResponseBody List<T> all();
+
+    /**
+     * Create a new object in the db
+     *
+     * @param t object to be saved
+     * @return saved object
+     */
+    default @ResponseBody T add(@RequestBody T t) {
+        return null;
+    };
+
+    @PostMapping(path="/add")
+    @ResponseBody
+    Item add(@RequestBody Item item, @RequestParam Long id) throws Exception;
+
+    /**
+     * delete any given object from the db
+     */
+    void delete(Long id);
+
+    /**
+     * update object in the db
+     *
+     * @param t updating object
+     * @return updated object
+     */
+    @ResponseBody T update(@RequestBody T t);
+
+    /**
+     * Get Object from db using given id
+     * @param id given id
+     * @return object retrieved from the db
+     */
+    @ResponseBody T getById(@RequestParam Long id);
+}
